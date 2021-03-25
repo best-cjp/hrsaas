2bda58cd628f18ec798380f04e1985eaf1a90949
best-cjp <185154740@qq.com>
2021-03-25 21:56:57 +0800

    通用工具栏的组件结构

---
+++ src/components/PageTools/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <el-card class="page-tools">
+    <el-row type="flex" justify="space-between" align="middle">
+      <el-col>
+        <div v-if="showBefore" class="before">
+          <i class="el-icon-info" />
+          <!-- 定义前面得插槽 -->
+          <slot name="before" />
+        </div>
+      </el-col>
+      <el-col>
+        <el-row type="flex" justify="end">
+          <!-- 定义后面的插槽 -->
+          <slot name="after" />
+        </el-row>
+      </el-col>
+    </el-row>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: '',
+  // 英 [kəm'pəʊnənts]  美 [kəm'ponənts]
+  // n. 部件；组件；成份（component复数）
+  components: {},
+  props: {
+    showBefore: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {}
+  },
+  // 计算
+  computed: {},
+  // 监听
+  watch: {},
+  // 实例创建后
+  created() {},
+  // 实例渲染后
+  mounted() {},
+  // 方法
+  methods: {}
+}
+</script>
+
+<style scoped lang="scss">
+.page-tools {
+  margin: 10px 0;
+  .before {
+    line-height: 34px;
+    i {
+      margin-right: 5px;
+      color: #409eff;
+    }
+    display: inline-block;
+    padding: 0px 10px;
+    border-radius: 3px;
+    border: 1px solid rgba(145, 213, 255, 1);
+    background: rgba(230, 247, 255, 1);
+  }
+}
+</style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,19 +1,40 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
+    <page-tools :show-before="true">
+      <template v-slot:before>
+        <span >我想你们</span>
+      </template>
+
+      <template v-slot:after>
+        <el-button slot="after" type="primary">导入excel</el-button>
+      </template>
+    </page-tools>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import PageTools from '@/components/PageTools'
 
 export default {
   name: 'Dashboard',
+  components: { PageTools },
+  props: {},
+  data() {
+    return {}
+  },
   computed: {
-    ...mapGetters([
-      'name'
-    ])
-  }
+    ...mapGetters(['name'])
+  },
+  // 监听
+  watch: {},
+  // 实例创建后
+  created() {},
+  // 实例渲染后
+  mounted() {},
+  // 方法
+  methods: {}
 }
 </script>
 
