b96dbc17834fead7e75504ff8b3cf789c21b830b
best-cjp <185154740@qq.com>
2021-03-28 11:49:15 +0800

    实现用户名和密码的修改

--- src/views/employees/detail.vue
+++ src/views/employees/detail.vue
@@ -6,17 +6,24 @@
           <el-tab-pane label="登录账户设置">
             <!-- 放置表单 -->
             <el-form
+              ref="userForm"
               label-width="120px"
               style="margin-left: 120px; margin-top:30px"
+              :rules="rules"
+              :model="userInfo"
             >
-              <el-form-item label="姓名:">
-                <el-input style="width:300px" />
+              <el-form-item label="姓名:" props="username">
+                <el-input v-model="userInfo.username" style="width:300px" />
               </el-form-item>
-              <el-form-item label="密码:">
-                <el-input style="width:300px" type="password" />
+              <el-form-item label="密码:" props="password2">
+                <el-input
+                  v-model="userInfo.password2"
+                  style="width:300px"
+                  type="password"
+                />
               </el-form-item>
               <el-form-item>
-                <el-button type="primary">更新</el-button>
+                <el-button type="primary" @click="saveUser">更新</el-button>
               </el-form-item>
             </el-form>
           </el-tab-pane>
@@ -29,6 +36,9 @@
 </template>
 
 <script>
+import { getUserDetailById } from '@/api/user'
+import { saveUserDetailById } from '@/api/employees'
+
 export default {
   name: 'Detail',
   // 英 [kəm'pəʊnənts]  美 [kəm'ponənts]
@@ -36,18 +46,48 @@ export default {
   components: {},
   props: {},
   data() {
-    return {}
+    return {
+      userId: this.$route.params.id,
+      userInfo: {
+        //   专门存放基本信息
+        username: '',
+        password2: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '姓名不能为空', trigger: 'blur' }
+        ],
+        password2: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 6, max: 9, message: '密码长度6-9位', trigger: 'blur' }
+        ]
+      }
+    }
   },
   // 计算
   computed: {},
   // 监听
   watch: {},
   // 实例创建后
-  created() {},
+  created() {
+    this.getUserDetailById()
+  },
   // 实例渲染后
   mounted() {},
   // 方法
-  methods: {}
+  methods: {
+    async getUserDetailById() {
+      this.userInfo = await getUserDetailById(this.userId)
+    },
+     saveUser() {
+      // 调用方法 校验
+      this.$refs.userForm.validate().then(async() => {
+        // 调用保存借口
+        await saveUserDetailById({...this.userInfo,password:this.userInfo.password2})
+        this.$message.success('保存成功')
+      })
+    }
+  }
 }
 </script>
 
