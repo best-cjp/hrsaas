7d3f7469132d4f295afa9f1f710d85403306355d
best-cjp <185154740@qq.com>
2021-03-28 18:55:12 +0800

    新建文件上传组件

---
+++ src/components/ImageUpload/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-upload list-type="picture-card">
+    <i class="el-icon-plus" />
+    
+  </el-upload>
+</template>
+
+<script>
+export default {
+  name: '',
+  // 英 [kəm'pəʊnənts]  美 [kəm'ponənts]
+  // n. 部件；组件；成份（component复数）
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  // 计算
+  computed: {},
+  // 监听
+  watch: {},
+  // 实例创建后
+  created() {},
+  // 实例渲染后
+  mounted() {},
+  // 方法
+  methods: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,14 @@ import PageTools from './PageTools/index.vue'
 
 import UploadExcel from './UploadExcel'
 
+import ImageUpload from './ImageUpload'
+
 // 导出并注册全局组件
 export default {
   install(Vue) {
     // 注册全局组件
     Vue.component('PageTools', PageTools) // 注册工具栏组件
     Vue.component('UploadExcel', UploadExcel) // 注册导入excel组件
+    Vue.component('ImageUpload', ImageUpload) // 注册图片上传组件
   }
 }
