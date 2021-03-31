958656c6dc2f3e9379905cd166382b9a2fc1512e
best-cjp <185154740@qq.com>
2021-03-31 11:44:11 +0800

    获取权限数据并转化树形

--- src/router/modules/permission.js
+++ src/router/modules/permission.js
@@ -2,7 +2,7 @@
 import layout from '@/layout'
 
 export default {
-  path: '/permission', //路由地址
+  path: '/', //路由地址
   component: layout, // 组件  //给路由规则加一个name
   name: 'permission',
   // 配置二级的路由表
--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -8,13 +8,13 @@
         </template>
       </page-tools>
       <!-- 表格 -->
-      <el-table border>
-        <el-table-column align="center" label="名称" />
-        <el-table-column align="center" label="标识" />
-        <el-table-column align="center" label="描述" />
+      <el-table border :data="list" row-key="id">
+        <el-table-column label="名称" prop="name" />
+        <el-table-column align="center" label="标识" prop="code" />
+        <el-table-column align="center" label="描述" prop="description" />
         <el-table-column align="center" label="操作">
-          <template>
-            <el-button type="text">添加</el-button>
+          <template v-slot="{ row }">
+            <el-button v-if="row.type === 1" type="text">添加</el-button>
             <el-button type="text">编辑</el-button>
             <el-button type="text">删除</el-button>
           </template>
@@ -25,6 +25,9 @@
 </template>
 
 <script>
+import { getPermissionList } from '@/api/permisson'
+import { tranListToTreeData } from '@/utils'
+
 export default {
   name: '',
   // 英 [kəm'pəʊnənts]  美 [kəm'ponənts]
@@ -32,18 +35,32 @@ export default {
   components: {},
   props: {},
   data() {
-    return {}
+    return {
+      list: []
+    }
   },
   // 计算
-  computed: {},
+  computed: {
+    showText() {
+      return this.formData.id ? '编辑' : '新增'
+    }
+  },
   // 监听
   watch: {},
   // 实例创建后
-  created() {},
+  created() {
+    this.getPermissionList()
+  },
   // 实例渲染后
   mounted() {},
   // 方法
-  methods: {}
+  methods: {
+    async getPermissionList() {
+      // 将数据转换成了带children的树形结构
+      this.list = tranListToTreeData(await getPermissionList(), '0')
+      // this.list = await getPermissionList()
+    }
+  }
 }
 </script>
 
